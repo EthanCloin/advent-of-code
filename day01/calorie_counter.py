8ba220d19eec65c53c4d71989d625b311891189d
Ethan Cloin <psnethan@gmail.com>
2022-12-01 21:36:29 -0500

    formatted w black

--- day01/calorie_counter.py
+++ day01/calorie_counter.py
@@ -1,26 +1,26 @@
-
 def heaviest_elf():
-    with open('day01/input.txt', 'r') as f:
+    with open("day01/input.txt", "r") as f:
         elf_calories = cur_max = 0
         for line in f.readlines():
-            if line == '\n':
+            if line == "\n":
                 # we know it's a new elf
                 # stop summing and check against maximum
                 cur_max = max(cur_max, elf_calories)
                 elf_calories = 0
             else:
-                item_calories = int(line.split('\n')[0])
+                item_calories = int(line.split("\n")[0])
                 elf_calories += item_calories
     return cur_max
+
 
 def top_three_heaviest_elves():
     # third <= second <= first
     first = second = third = 0
 
-    with open('day01/input.txt', 'r') as f:
+    with open("day01/input.txt", "r") as f:
         elf_calories = 0
         for line in f.readlines():
-            if line == '\n':
+            if line == "\n":
                 # smaller than third largest
                 if elf_calories <= third:
                     pass
@@ -36,16 +36,15 @@ def top_three_heaviest_elves():
                     second = elf_calories
 
                 # new largest
-                else: 
+                else:
                     third = second
                     second = first
                     first = elf_calories
                 elf_calories = 0
             else:
-                elf_calories += int(line.split('\n')[0])
+                elf_calories += int(line.split("\n")[0])
         print(f"1st: {first} -- 2nd: {second} -- 3rd: {third}")
         return first + second + third
 
+
 print(top_three_heaviest_elves(), heaviest_elf())
-
-
